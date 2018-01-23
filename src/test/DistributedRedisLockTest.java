bc61eccc5c76146bdcc238f409ad019b392c6209
zhuhaiyun <zhuhaiyun@meituan.com>
2018-01-23 23:04:38 +0800

    testredis

--- src/test/DistributedRedisLockTest.java
+++ src/test/DistributedRedisLockTest.java
@@ -6,14 +6,13 @@ import utils.RedisUtils;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 
-import static javafx.scene.input.KeyCode.R;
 
 /**
  * Created by zhuhaiyun on 2018/1/21.
  */
 public class DistributedRedisLockTest {
-    public static void main(String[] args) throws Exception{
-        String host = "172.0.0.1";
+    public static void main(String[] args) throws Exception {
+        String host = "127.0.0.1";
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
         //最小空闲连接数
         jedisPoolConfig.setMinIdle(10);
@@ -21,33 +20,35 @@ public class DistributedRedisLockTest {
         jedisPoolConfig.setMaxIdle(18);
         //设置最大连接数
         jedisPoolConfig.setMaxTotal(150);
-        JedisPool jedisPool = new JedisPool(jedisPoolConfig,host,6379);
-        //// zhuhaiyun-下午10:04 TODO: 了解CountDownLatch
-        CountDownLatch countDownLatch = new CountDownLatch(100);
-        for (int i = 0; i < 100; i++) {
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, 6379);
+        //防止死锁
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        for (int i = 0; i < 5; i++) {
 //            UUID uuid = UUID.randomUUID();
-            new Thread(()->{
+            new Thread(() -> {
                 String uuid = UUID.randomUUID().toString();
-                String key = "age";
+                String key = "name";
                 Jedis jedis = jedisPool.getResource();
-                try {
-                    boolean result = false;
-                    do {
-                        RedisUtils.lock(jedis, key, uuid, 30);
-                    } while (result == false);
-                    //// zhuhaiyun-下午10:25 TODO: 含义是什么
-                    int value = Integer.valueOf(jedis.get("value"));
-                    jedis.set("value", ++value + "");
+                String value = "helloRedis";
+                Boolean lock = RedisUtils.lock(jedis, key, value, 1);
+                try{
+                    if (lock) {
+                        System.out.println(Thread.currentThread().getName()+"lock key = "+ key + " success!");
+//                        Thread.sleep(200);
+                    } else {
+                        System.out.println(Thread.currentThread().getName() +"lock key = " + key + " fail!");
+                    }
+                }catch (Exception e) {
+
                 }finally {
-                    jedis.close();
-                    RedisUtils.unlock(jedis,key,uuid);
+                    RedisUtils.unlock(jedis,key,value);
                     countDownLatch.countDown();
                 }
+
             }).start();
         }
         countDownLatch.await();
-        Jedis jedis = new Jedis(host,6379);
-        int value = Integer.parseInt(jedis.get("value"));
-        System.out.println("value="+value);
+        //多线程执行完之后执行主线程后面的逻辑
+        System.out.println("--------hello-------");
     }
 }
